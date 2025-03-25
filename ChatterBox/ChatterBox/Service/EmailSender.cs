46a335734a5643d10af150b10d12e386bc5a3062
Kalin1603 <kalintodorov03@gmail.com>
2025-03-25 11:48:25 +0200

    Added EmailSender.cs

---
+++ ChatterBox/ChatterBox/Service/EmailSender.cs
@@ -0,0 +1,51 @@
+﻿using ChatterBox.Models;
+using Microsoft.AspNetCore.Identity.UI.Services;
+using Microsoft.Extensions.Options;
+using System.Net.Mail;
+using System.Net;
+
+namespace ChatterBox.Service
+{
+    public class EmailSender : IEmailSender
+    {
+        private readonly EmailSettings _emailSettings;
+
+        public EmailSender(IOptions<EmailSettings> emailSettingsOptions)
+        {
+            _emailSettings = emailSettingsOptions.Value;
+        }
+
+        public async Task SendEmailAsync(string email, string subject, string htmlMessage)
+        {
+            using (var client = new SmtpClient(_emailSettings.SmtpHost, _emailSettings.SmtpPort))
+            {
+                client.UseDefaultCredentials = false;
+                client.Credentials = new NetworkCredential(_emailSettings.SmtpUser, _emailSettings.SmtpPass);
+                client.EnableSsl = true;
+
+                var mailMessage = new MailMessage
+                {
+                    From = new MailAddress(_emailSettings.FromEmail, "ChatterBox"),
+                    Subject = subject,
+                    IsBodyHtml = true,
+                    Body = htmlMessage
+
+                };
+
+                mailMessage.To.Add(email);
+
+                try
+                {
+                    Console.WriteLine($"Sending email to {email}...");
+                    await client.SendMailAsync(mailMessage);
+                    Console.WriteLine("Email sent successfully!");
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"Error sending email: {ex.Message}");
+                }
+
+            }
+        }
+    }
+}
